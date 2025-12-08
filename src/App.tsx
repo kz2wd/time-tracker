e4ab72e443198d2cd7333197579d99f7952737af
Youri Chancrin <ychancrin@bordeaux-inp.fr>
2025-12-08 21:27:54 +0100

    Base version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState, createContext, useContext } from 'react'
+import { useEffect, useState, createContext, useContext, useRef } from 'react'
 import './App.css'
-import { database, Task } from './database'
+import { database, Task, WorkEntry } from './database'
 
 
 // Work Entry
@@ -11,32 +11,28 @@ import { database, Task } from './database'
 // Note (int / 5)
 
 
-const mockDescriptions = [
-  "Buy groceries",
-  "Finish report",
-  "Clean desk",
-  "Plan trip",
-  "Fix bug #42",
-  "Read documentation",
-  "Water plants",
-  "Call client",
-  "Organize files",
-  "Review pull request"
-]
-
-function choice<T>(arr: T[]): T {
-  return arr[Math.floor(Math.random() * arr.length)]
-}
-
-function generateFakeTasks(n: number): Task[] {
-  return Array(n).map(() => new Task(choice(mockDescriptions)))
-}
-
-
-function TaskCard({ task }: { task: Task }) {
+function TaskCard({ task, hideOldTask }: { task: Task, hideOldTask: (t: Task) => void }) {
   const tskctx = useContext(SelectedTaskContext)!
-  const classes = `task-card ${tskctx.selectedTask == task ? 'selected-task' : ''}`
-  
+  const classes = `task-card task ${tskctx.selectedTask == task ? 'selected-task' : ''}`
+  const [draft, setDraft] = useState(task.description.toString())
+
+  function confirmEdit() {
+    if (draft === "") {
+      hideOldTask(task)
+      task.delete()
+      tskctx.setSelectedTask(null)
+    } else {
+      task.setDescription(draft)
+    }
+    tskctx.setEditedTask(null)
+  }
+
+  function cancelEdit() {
+    setDraft(task.description.toString())
+    tskctx.setEditedTask(null)
+  }
+
+
   return (
     <>
       <div className={classes}
@@ -44,62 +40,129 @@ function TaskCard({ task }: { task: Task }) {
             tskctx.setSelectedTask(task)
           }}
         onDoubleClick={() => {
-          console.log("double")
+           tskctx.setEditedTask(task)
         }}
       >
-        {task.description}
+        
+        <input
+          value={draft as string}
+          onBlur={() => {
+            confirmEdit()
+          }}
+          onChange={(ev) => setDraft(ev.target.value)}
+          onKeyDown={(ev) => {
+            if (ev.key === "Enter") confirmEdit()
+            if (ev.key === "Escape") cancelEdit()
+          }}
+          className="task-input"
+        /> 
+        
       </div>
     </>
   )
 }
 
-const rootTasks: Task[] = [
-    new Task(choice(mockDescriptions)),
-    new Task(choice(mockDescriptions)),
-    new Task(choice(mockDescriptions)),
-  ]
+function AddCard({ showNewTask }: { showNewTask: (t: Task) => void }) {
+  const [draft, setDraft] = useState("")
+  async function addTask() {
+    // replace null with selected task
+    const task = await (await database).addTask(draft, null)
+    showNewTask(task)
+  }
+
+  return (
+    <>
+      <div className="task-card task">
+        <input placeholder='new task' className="task-input"
+          value={draft}
+          onKeyDown={(ev) => {
+            if (ev.key === "Enter") {
+              addTask()
+              setDraft("")
+            }
+          }}
+        onChange={(ev) => {
+          setDraft(ev.target.value)
+        }}
+        
+        />
+      </div>
+    </>
+  )
+} 
 
 function TaskContainer() {
 
   // Fetch root level tasks
-
+  const [rootTasks, setRootTasks] = useState<Array<Task>>([])
   
+  useEffect(() => {
+
+    async function load() {
+      const tasks = await (await database).getRootTasks() 
+      setRootTasks(tasks)  
+    }
+
+    load()
+    
+  }, [])
+
+  function showNewTask(task: Task) {
+      setRootTasks([...rootTasks, task]);
+  }
+
+  function hideOldTask(task: Task) {
+    setRootTasks(rootTasks.filter((t: Task) => t.id !== task.id));
+  }
 
   return (
     <>
       <div className='task-container'>
         {rootTasks.map((it, index) => (
-          <TaskCard 
+          <TaskCard
             key={it.id || index} 
             task={it}
-            />
+            hideOldTask={hideOldTask}
+          />
         ))}
+        <AddCard
+          showNewTask={showNewTask}
+         />
       </div>
     </>
   )
 }
 
-function minutesToHoursMinutes(minutes: number) {
+function convertTime(seconds: number) {
   return {
-    hours: Math.floor(minutes / 60),
-    minutes: minutes % 60,
+    hours: Math.floor(seconds / 3600),
+    minutes: Math.floor(seconds % 3600 / 60) ,
+    seconds: Math.floor(seconds % 60),
   };
 }
 
 
-function TimeDisplay({ totalMinutes, text }: {
-  totalMinutes: number, text: String
-}) {
-  const { hours, minutes } = minutesToHoursMinutes(totalMinutes);
+function TimeDisplay({ timeFetch, text }: {
+  timeFetch: () => Promise<number> , text: String
+  }) {
+  const [timeSeconds, setTimeSeconds] = useState(0)
+
+  useEffect(() => {
+    timeFetch().then(setTimeSeconds)
+  }, [])
+
   return (
     <>
       <div className='time-display'>
         <div>
-          <span className="hours-display" >{hours}</span>
-          <span className="h-label">H</span>
-          <span className="minutes-display">{minutes}</span>
+          
+          <span className="hours-display science-gothic-font" >{convertTime(timeSeconds).hours.toString().padStart(2, '0')}</span>
+          <span className="h-label science-gothic-font">:</span>
+          <span className="minutes-display science-gothic-font">{convertTime(timeSeconds).minutes.toString().padStart(2, '0')}</span>
+          <span className="h-label science-gothic-font">:</span>
+          <span className="minutes-display science-gothic-font">{convertTime(timeSeconds).seconds.toString().padStart(2, '0')}</span>
         </div>
-        <p>{text}</p>
+        <p className="playwrite-no-font">{text}</p>
       </div>      
     </>
   )
@@ -108,19 +171,29 @@ function TimeDisplay({ totalMinutes, text }: {
 function BottomBar() {
 
   const tskctx = useContext(SelectedTaskContext)!  
-
+  const [currentWorkEntry, setCurrentWorkEntry] = useState<WorkEntry | null>(null)
   return (
     <>
       <div className="bottom-bar">
-        {tskctx.selectedTask ? tskctx.selectedTask.description : "Select task"}
+        <div className="selected-task-panel task">
+          {tskctx.selectedTask ? tskctx.selectedTask.description : "Select task"}
+        </div>
         <div className="control-panel">
-          <TimeDisplay totalMinutes={140} text={"Total"} />
-          <button onClick={() => {
-              tskctx.setIsWorking(!tskctx.isWorking)
+          <TimeDisplay timeFetch={async () => await (await database).getWorkTimeSeconds(null, tskctx.selectedTask?.id)}
+            text={"Total"} />
+          <button onClick={async () => {
+            if (!tskctx.isWorking) {
+              const workEntry = await (await database).addWorkEntry(tskctx.selectedTask)
+              setCurrentWorkEntry(workEntry)
+            } else if (currentWorkEntry != null) {
+              currentWorkEntry.finish()
+            }
+            tskctx.setIsWorking(!tskctx.isWorking)
             }
           } className={tskctx.isWorking ? "stop": "start"}>
             {tskctx.isWorking ? "Stop": "Start"}</button>
-          <TimeDisplay totalMinutes={10} text={"Today"} />
+          <TimeDisplay timeFetch={async () => await (await database).getWorkTimeSeconds(24, tskctx.selectedTask?.id)}
+          text={"Today"} />
         </div>
       </div>
     </>
@@ -133,11 +206,14 @@ const SelectedTaskContext = createContext<{
   setSelectedTask: (t: Task | null) => void
   isWorking: boolean
   setIsWorking: (b: boolean) => void
+  editedTask: Task | null
+  setEditedTask: (t: Task | null) => void
 } | null>(null); 
 
 function App() {
   const [selectedTask, setSelectedTask] = useState<Task | null>(null)
   const [isWorking, setIsWorking] = useState(false)
+  const [editedTask, setEditedTask] = useState<Task | null>(null)
   
   const customSetSelectedTask = (task: Task | null) => {
     if (task == selectedTask) return
@@ -151,7 +227,10 @@ function App() {
         selectedTask: selectedTask, 
         setSelectedTask: customSetSelectedTask, 
         isWorking: isWorking,
-        setIsWorking: setIsWorking }}>
+        setIsWorking: setIsWorking,
+        editedTask: editedTask,
+        setEditedTask: setEditedTask,
+      }}>
       <TaskContainer />
       <BottomBar />
     </SelectedTaskContext.Provider>
--- src/database.ts
+++ src/database.ts
@@ -11,18 +11,42 @@ export class Task {
         this.parentId = parentId
     }
 
+    toObject() {
+        return {
+            // do not include id as IndexedDB will handle it
+            description: this.description,
+            parentId: this.parentId,
+            subtaskIds: this.subtaskIds,
+            isOver: this.isOver,
+        }
+    }
+
+    static fromObject(o: any): Task {
+        const t = new Task(o.description, o.parentId)
+        t.id = o.id ?? undefined
+        t.subtaskIds = o.subtaskIds ?? []
+        t.isOver = o.isOver ?? false
+        return t
+    }
+
     async setDescription(newDescription: String) {
-        this.description = newDescription
-        database.updateTask(this)
+        this.description = newDescription;
+        (await database).updateTask(this);
     }
 
     async setIsOver(newIsOver: boolean) {
-        this.isOver = newIsOver
-        database.updateTask(this)
+        this.isOver = newIsOver;
+        (await database).updateTask(this);
     }
 
     async getTotalTimeMinutes(): Promise<number> {
         return 10
+    }
+    async delete(): Promise<void> {
+        if (this.id === null) return
+
+        // Check if task has work entry, if so, ask if user is sure
+        (await database).deleteTask(this.id!)
     }
 
 }
@@ -39,15 +63,32 @@ export class WorkEntry {
         this.relatedTaskId = relatedTaskId
     }
 
+    toObject() {
+        return {
+            start: this.start,
+            end: this.end,
+            relatedTaskId: this.relatedTaskId,
+            satisfaction: this.satisfaction,
+        }
+    }
+
+    static fromObject(o: any): WorkEntry {
+        const wo = new WorkEntry(o.relatedTaskId)
+        wo.start = o.start  // Idk how to handle this one so i'd rather let that crash
+        wo.end = o.end ?? null
+        wo.satisfaction = o.satisfaction ?? null
+        return wo
+    }
+
     async finish() {
-        this.end = Date.now()
-        await database.updateWorkEntry(this)
+        this.end = Date.now();
+        (await database).updateWorkEntry(this);
     }
 
     async setSatisfaction(satisfaction: number) {
-        satisfaction = Math.min(Math.max(satisfaction, 0), 5)
-        this.satisfaction = satisfaction
-        database.updateWorkEntry(this)
+        satisfaction = Math.min(Math.max(satisfaction, 0), 5);
+        this.satisfaction = satisfaction;
+        (await database).updateWorkEntry(this);
     }
 
 }
@@ -57,9 +98,26 @@ let connection: IDBDatabase
 function request<T>(req: IDBRequest<T>): Promise<T> {
     return new Promise((resolve, reject) => {
         req.onsuccess = () => resolve(req.result)
-        req.onerror = () => reject(req.error)
+        req.onerror = () => {
+            console.log(req.error?.message)
+            reject(req.error)
+        }
     })
 }
+
+async function* cursorIterator(store: IDBObjectStore) {
+    let cursorRequest = store.openCursor()
+    while (true) {
+        const cursor: IDBCursorWithValue | null = await new Promise((resolve, reject) => {
+            cursorRequest.onsuccess = () => resolve(cursorRequest.result)
+            cursorRequest.onerror = () => reject(cursorRequest.error)
+        })
+        if (!cursor) break
+        yield cursor
+        cursor.continue()
+    }
+}
+
 
 async function updateParent(store: IDBObjectStore, childId: number, parent: Task): Promise<void> {
     parent.subtaskIds.push(childId)
@@ -68,8 +126,8 @@ async function updateParent(store: IDBObjectStore, childId: number, parent: Task
 }
 
 async function initDatabase (): Promise<Database> {
-    await new Promise((resolve, reject) => {
-        let openRequest = indexedDB.open("TasksTrackerDB", 1)
+    return new Promise((resolve, reject) => {
+        let openRequest = indexedDB.open("TasksTrackerDB", 3)
 
         openRequest.onerror = (_event: Event) => {
             console.log("Usage of IndexedDB not allowed.")
@@ -103,9 +161,7 @@ async function initDatabase (): Promise<Database> {
                 )
                 objectStore.createIndex("relatedTaskId", "relatedTaskId")
             }
-
         }
-
     })
 }
 
@@ -113,41 +169,57 @@ export const database: Promise<Database> = initDatabase()
 
 interface Database {
     addTask(description: String, parent: Task | null): Promise<Task>
+    getRootTasks(): Promise<Array<Task>>
     updateTask(task: Task): Promise<void>
+    deleteTask(taskId: number): Promise<void>
     addWorkEntry(task: Task | null): Promise<WorkEntry>
     updateWorkEntry(workEntry: WorkEntry): Promise<void>
+    getWorkTimeSeconds(sinceXHours?: number | null, taskId?: number | null): Promise<number>
 }
 
 const databaseCore: Database = {
     async addTask(description: string, parent: Task | null): Promise<Task> {
-        const task: Task = new Task(
+        let task: Task = new Task(
             description = description,
             parent?.id
         )
         const transaction = connection.transaction("tasks", "readwrite")
         const store = transaction.objectStore("tasks")
-        const rq = store.add(task)
-
-        const key = await request<IDBValidKey>(rq)
-        task.id = key as number
-
-        if (parent != null) {
-            await updateParent(store, key as number, parent)
+        let rq: IDBRequest<IDBValidKey>
+        try {
+            rq = store.add(task.toObject())
+            const key = await request<IDBValidKey>(rq)
+            task.id = key as number
+            if (parent != null) {
+                await updateParent(store, key as number, parent)
+            }
+        } catch (e) {
+            console.error(e, task);
         }
+        
         return task
     },
-
+    async getRootTasks(): Promise<Array<Task>> {
+        const transaction = connection.transaction("tasks", "readonly")
+        const store = transaction.objectStore("tasks")
+        return request(store.getAll()).then(arr => arr.map(it => Task.fromObject(it)))
+    },
     async updateTask(task: Task): Promise<void> {
         const transaction = connection.transaction("tasks", "readwrite")
         const store = transaction.objectStore("tasks")
         await request(store.put(task))
+    },
+    async deleteTask(taskId: number): Promise<void> {
+        const transaction = connection.transaction("tasks", "readwrite")
+        const store = transaction.objectStore("tasks")
+        await request(store.delete(taskId))
     },
 
     async addWorkEntry(task: Task | null = null): Promise<WorkEntry> {
         const workEntry: WorkEntry = new WorkEntry(task?.id)
         const transaction = connection.transaction("workEntries", "readwrite")
         const store = transaction.objectStore("workEntries")
-        const req = store.add(workEntry)
+        const req = store.add(workEntry.toObject())
         const id = await request(req) as number
         workEntry.id = id
         return workEntry
@@ -157,6 +229,23 @@ const databaseCore: Database = {
         const transaction = connection.transaction("workEntries", "readwrite")
         const store = transaction.objectStore("workEntries")
         await request(store.put(workEntry))
+    },
+    async getWorkTimeSeconds(sinceXHours: number | null = null, taskId: number | null = null): Promise<number> {
+        const transaction = connection.transaction("workEntries", "readonly")
+        const store = transaction.objectStore("workEntries")
+        const cutoff = sinceXHours !== null ? Date.now() - sinceXHours * 60 * 60 * 1000 : null
+        let totalMs = 0
+
+        for await (const cursor of cursorIterator(store)) {
+            const entry: WorkEntry = WorkEntry.fromObject(cursor.value)
+            if (taskId && entry.id != taskId) {
+                continue
+            }
+            if (!cutoff || entry.start >= cutoff) {
+                totalMs += entry.end ? (entry.end - entry.start) : (Date.now() - entry.start)
+            }
+        }
+        return totalMs / 1000
     }
 
 } 
